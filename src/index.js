01146f6a0146eaff0026f067ea29f0c7856f2418
muyanpan <muyan@live.unc.edu>
2021-02-24 22:02:59 -0500

    fixed image movement + restructure file layout for phaser scenes

--- src/index.js
+++ src/index.js
@@ -11,80 +11,10 @@ export const config = {
   parent: "phaser",
   width: 800,
   height: 800,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+  scene: playGame
 };
 
-let follower;
-let path;
-let graphics;
-
-function preload() {
-  this.load.image("Baby", CharlieBrown);
-}
-
-function create ()
-{
-  graphics = this.add.graphics();   
-  follower = { t: 0, vec: new Phaser.Math.Vector2() }; 
-    
-    // the path for our enemies
-    // parameters are the start x and y of our path
-    path = this.add.path(50, 800);
-    path.lineTo(50, 550);
-    path.lineTo(300, 550);
-    path.lineTo(300, 300);
-    path.lineTo(500, 300);
-    path.ellipseTo(200, 100, 90, 270, true);
-    path.lineTo(600, 500);
-    path.ellipseTo(150, 150, 0, 360, false, 315);
-    path.lineTo(500, 605);
-
-    var placeHolder = this.tweens.add({
-      targets: follower,
-      t: 1,
-      ease: 'Sine.easeInOut',
-      duration: 30000,
-      yoyo: false,
-      repeat: -1
-    });
-
-}
-
-function update ()
-{
-
-  graphics.clear();
-  graphics.lineStyle(2, 0xffffff, 1);
-
-  path.draw(graphics);
-
-  path.getPoint(follower.t, follower.vec);
-
-  graphics.fillStyle(0xff0000, 1);
-  graphics.fillCircle(follower.vec.x, follower.vec.y, 1);
-  fillHazards();
-  var newBaby = this.add.image(follower.vec.x, follower.vec.y, "Baby");
-  newBaby.setScale(0.4);
-  setTimeout(() => {  newBaby.destroy(); }, 1);
-}
-
-function fillHazards() {
-  graphics.fillCircle(50, 550, 30);
-  graphics.fillStyle(0x07e000, 1);
-  graphics.fillCircle(300, 550, 30);
-  graphics.fillCircle(300, 300, 30);
-  graphics.fillStyle(0xff0000, 1);
-  graphics.fillCircle(500, 300, 30);
-  graphics.fillStyle(0x07e000, 1);
-  graphics.fillCircle(600, 500, 30);
-}
-
 var game = new Phaser.Game(config);
-
 
 ReactDOM.render(
   <App />,
--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -1,23 +1,63 @@
 import Phaser from "phaser";
 import CharlieBrown from "../assets/CharlieBrown.jpg"
 
+let toddler1;
+let graphics;
+let path;
+let placeHolder;
+let spawnEvent;
+
+let toddlerList;
+let graphicsList;
+let pathList;
+let placeHolderList;
+
+
 class playGame extends Phaser.Scene {
   constructor() {
     super("PlayGame");
   }
   preload() {
-    // this.load.image("Baby", CharlieBrown);
+    this.load.image("Baby", CharlieBrown);
   }
   create() {
-    // const baby = this.add.image(400, 150, "Baby");
-    // this.tweens.add({
-    //   targets: baby,
-    //   y: 450,
-    //   duration: 2000,
-    //   ease: "Power2",
-    //   yoyo: true,
-    //   loop: -1
-    // });
+    toddler1 = this.add.image(50, 800, "Baby").setScale(0.4);
+    graphics = this.add.graphics();
+    placeHolder = { t: 0, vec: new Phaser.Math.Vector2() }; 
+    //spawnEvent = this.time.addEvent({ delay: 3000, callback: onSpawn, callbackScope: this, repeat: 10})
+
+    this.createTrack();
+
+    graphics.lineStyle(2, 0xffffff, 1);
+    path.draw(graphics);
+
+    let marker = this.tweens.add({
+      targets: placeHolder,
+      t: 1,
+      ease: 'Sine.easeInOut',
+      duration: 30000,
+      yoyo: false,
+      repeat: -1
+    });
+  }
+  update() {
+    this.moveToddler(toddler1, placeHolder);
+  }
+  createTrack() {
+    path = this.add.path(50, 800);
+    path.lineTo(50, 550);
+    path.lineTo(300, 550);
+    path.lineTo(300, 150);
+    path.lineTo(700, 230);
+    path.lineTo(700, 800);
+  }
+  moveToddler(toddler, placeHolder) {
+    path.getPoint(placeHolder.t, placeHolder.vec);
+    toddler.x = placeHolder.vec.x;
+    toddler.y = placeHolder.vec.y;
+  }
+  onSpawn() {
+
   }
 }
 
