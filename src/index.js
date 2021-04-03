ae36f95a611cb17db8b7b352aecc70f4661baac4
evanfalcinelli <44810036+evanfalcinelli@users.noreply.github.com>
2021-04-03 19:09:19 -0400

    added fit to window scaling

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ export const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
   physics: {
     default: "arcade",
     arcade: {
